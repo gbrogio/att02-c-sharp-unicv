2a371044a9121cbc2659a5871e22783daad6a78b
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-02-23 10:43:21 -0300

    Ex35. Lista em ordem Decrescente

---
+++ Exercicios/Ex35_OrdemDecrescente.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int> {5, 1, 3, 2, 4};
+            List<int> lista2 = new List<int> { };
+
+            lista2.Add(10);
+            lista2.Add(8);
+            lista2.Add(9);
+            lista2.Add(6);
+            lista2.Add(7);
+
+            Console.WriteLine("------------35-----------");
+            
+            lista2.Sort();
+            lista2.Reverse();
+            foreach (var item35 in lista2)
+            {
+                Console.WriteLine(item35);
+            }
+
+        }
+    }
+}
