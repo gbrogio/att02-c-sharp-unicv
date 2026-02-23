e5f5ddb1a375a252003fe5ea8dc5ef483556a2a7
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-02-23 10:46:05 -0300

    Ex37. Buscar a nota pelo nome

---
+++ Exercicios/Ex37_BuscarNota.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          Dictionary<string, int> alunoNotas = new Dictionary<String, int>();
+          
+          alunoNotas.Add("João", 8);
+          alunoNotas.Add("Maria", 9);
+          alunoNotas.Add("Pedro", 7);
+          alunoNotas.Add("Ana", 10);
+          
+            Console.WriteLine("------------37-----------");
+
+            if (alunoNotas.TryGetValue("Ana", out int valor))
+            {
+                Console.WriteLine("A nota e: " + valor);
+            }
+	
+
+        }
+    }
+}
