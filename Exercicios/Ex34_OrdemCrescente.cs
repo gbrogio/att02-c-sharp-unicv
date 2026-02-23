3453759ec5896c6af7eea5f6e8b1458ed5d32c66
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-02-23 10:42:30 -0300

    Ex34. Valores em ordem crescente
    Sorts a list of integers and prints them to the console.

---
+++ Exercicios/Ex34_OrdemCrescente.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int> {5, 1, 3, 2, 4};
+            List<int> lista2 = new List<int> { };
+
+            lista2.Add(10);
+            lista2.Add(8);
+            lista2.Add(9);
+            lista2.Add(6);
+            lista2.Add(7); 
+
+            Console.WriteLine("------------34-----------");
+            
+            lista.Sort();
+            //Console.WriteLine(lista);
+            foreach (var item34 in lista)
+            {
+                Console.WriteLine(item34);
+            }
+
+        }
+    }
+}
