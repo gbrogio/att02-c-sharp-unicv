8b58e7dbcfecf2df471a79ab3dcded66b09355c3
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-02-23 10:39:29 -0300

    Ex33. Valida se existe o elemento
    Implement a program to check if a number exists in a list.

---
+++ Exercicios/Ex33_BuscaValor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int> {5, 1, 3, 2, 4};
+            List<int> lista2 = new List<int> { };
+
+            lista2.Add(10);
+            lista2.Add(8);
+            lista2.Add(9);
+            lista2.Add(6);
+            lista2.Add(7); 
+            Console.WriteLine("------------33-----------");
+            
+            if (lista.Contains(3))
+            {
+                Console.WriteLine("O número 3 esta na lista.");
+            }
+            else
+            {
+                Console.WriteLine("O número 3 não esta na lista.");
+            }
+
+        }
+    }
+}
